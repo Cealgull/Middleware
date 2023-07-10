81fa4970e5e590ada21039eea4f74477f3d1b5e8
Sharpx <sharpx-@outlook.com>
2023-07-10 17:09:20 +0800

    feat: support upload string
    Add support for uploading string.

--- ipfs/ipfs.go
+++ ipfs/ipfs.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 
 	shell "github.com/ipfs/go-ipfs-api"
 	"github.com/labstack/echo/v4"
@@ -17,8 +18,23 @@ func Init(url string) {
 	sh = shell.NewShell(url)
 }
 
-func Upload(c echo.Context) error {
-	fmt.Println("Upload Endpoint Hit")
+func UploadString(c echo.Context) error {
+	fmt.Println("Upload String Endpoint Hit")
+
+	input := c.FormValue("input")
+	fmt.Println("Input:", input)
+
+	cid, err := sh.Add(strings.NewReader(input))
+	if err != nil {
+		return err
+	}
+	fmt.Println("CID:", cid)
+
+	return c.String(http.StatusOK, cid)
+}
+
+func UploadFile(c echo.Context) error {
+	fmt.Println("Upload File Endpoint Hit")
 
 	fileHeader, err := c.FormFile("file")
 	if err != nil {
