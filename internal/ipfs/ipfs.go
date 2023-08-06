fb4d17c4926c84443ad1d02f7f1d40253e464dd4
Yiyang Wu <toolmanp@outlook.com>
2023-08-06 22:27:43 +0800

    feat(ipfs): impl cat semantics
    Use IPFS Shell to impl cat semantics
    
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

--- internal/ipfs/ipfs.go
+++ internal/ipfs/ipfs.go
@@ -25,7 +25,7 @@ func NewIPFSManager(logger *zap.Logger, url string) (*IPFSManager, error) {
 
 	if err != nil {
 		logger.Error("Error happened when initializing IPFS", zap.String("Error", err.Error()))
-		return nil, backendError
+		return nil, ipfsBackendError
 	}
 
 	mgr.sh = sh
@@ -37,7 +37,17 @@ func NewIPFSManager(logger *zap.Logger, url string) (*IPFSManager, error) {
 func (m *IPFSManager) Put(payload io.Reader) (string, proto.MiddlewareError) {
 	cid, err := m.sh.Add(payload)
 	if err != nil {
-		return "", backendError
+		return "", ipfsBackendError
 	}
 	return cid, nil
+}
+
+func (m *IPFSManager) Cat(cid string) ([]byte, proto.MiddlewareError) {
+	r, err := m.sh.Cat(cid)
+	if err != nil {
+		return nil, &IPFSFileNotFoundError{}
+	}
+	data, _ := io.ReadAll(r)
+
+	return data, nil
 }
