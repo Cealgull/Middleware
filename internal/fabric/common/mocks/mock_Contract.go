caee762f358759d6fd4ca1f2717dc79b4bec900c
Yiyang Wu <toolmanp@outlook.com>
2023-08-18 22:44:37 +0800

    test(mock): add contract mock
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

---
+++ internal/fabric/common/mocks/mock_Contract.go
@@ -0,0 +1,28 @@
+package mocks
+
+import (
+	"github.com/hyperledger/fabric-gateway/pkg/client"
+	"github.com/stretchr/testify/mock"
+)
+
+//Custom type alias is not supported by package loading.
+//This Mock should be implemented manually.
+//See https://github.com/vektra/mockery/issues/331
+
+type MockContract struct {
+	mock.Mock
+}
+
+func (m *MockContract) Submit(transactionName string, options ...client.ProposalOption) ([]byte, error) {
+	args := m.Called(transactionName, options)
+	return args.Get(0).([]byte), args.Error(1)
+}
+
+func (m *MockContract) ChaincodeName() string {
+	args := m.Called()
+	return args.String(0)
+}
+
+func NewMockContract() *MockContract {
+	return &MockContract{}
+}
